88b3d338f9c5ae021ff0e0f82fa4da6a9475acb0
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-10-31 03:07:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write(
+    "在程式設計中，對於字串文字的處理是個免不了的工作。對於處理字串，Python內建各種不同的工具，本篇要討論的是關於Python處理字串格式化的各種語法。 [yahoo](https://tw.yahoo.com)."
+)
